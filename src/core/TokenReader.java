8f552926e2305344018b6cffb36cfe422302c2ed
Mateus Resende <mateus.rsnd@gmail.com>
2016-06-21 22:31:39 -0300

    Switch

--- src/core/TokenReader.java
+++ src/core/TokenReader.java
@@ -1,13 +1,57 @@
 package core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by mresende on 21/06/16.
  */
 public class TokenReader {
 
+    private List<Token> results = null;
 
-    public String read(String token) {
+    public TokenReader() {
+        results = new ArrayList<Token>();
+    }
 
+    public String read(String input) {
+        int pos = 0;
+        int state = 0;
+        
+        while(input.length() < pos ) {
+            Character c = input.charAt(pos);
+            switch (state) {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    break;
+                default:
+                    break;
+            }
+            pos++;
+        }
         return null;
     }
 }
